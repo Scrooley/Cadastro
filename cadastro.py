36358ae4e205c4ae56b3d14ef54aab6912d8e78d
Lucas Santos <100425690+Scrooley@users.noreply.github.com>
2023-03-04 19:17:45 -0300

    Add files via upload

---
+++ cadastro.py
@@ -0,0 +1,19 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuario'), sg.Input(key='usuario', size=(36,2))],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(36,2))],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
+
+janela = sg.Window('Tela de login', layout)
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'lucas' and valores['senha'] == '123456':
+            print ('Bem-vindo!')
